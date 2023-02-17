ea85f004dc899be174e1e759a8196bf779469eab
Lasse <lasse.kotz@outlook.com>
2023-02-17 20:06:42 +0100

    Update shit

--- main.py
+++ main.py
@@ -2,32 +2,77 @@ import pygame
 import sys
 pygame.init()
 
-size = width, height = 1536, 864
-black = 0,0,0
+size = width, height = 494*2, 684
 
 screen = pygame.display.set_mode(size)
-#map = pygame.image.load("map.png")
 pin = pygame.image.load("pin.png")
-ballrect = pin.get_rect()
-
-while True: #run game
-    #TODO: Load images and labels (true coordinates)
-    for image in images: ##TODO: Loop over all images
-        #TODO: Random sample image
-        #TODO: display image
-        #TODO: Display map
-
-        #TODO: Move mechanics for pin
+pin = pygame.transform.scale(pin, (20, 20))
+map = pygame.image.load("map2.png")
+map = pygame.transform.scale(map, (494, 684))
 
 
+rectangle = pin.get_rect()
 
-        pass
+WHITE = (255, 255, 255)
+RED   = (255,   0,   0)
+FPS = 30
 
+rectangle_draging = False
+
+# - mainloop -
+
+clock = pygame.time.Clock()
+
+running = True
+
+while running:
+
+    # - events -
 
     for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
+
+        if event.type == pygame.QUIT:
+            running = False
+
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                if rectangle.collidepoint(event.pos):
+                    rectangle_draging = True
+                    mouse_x, mouse_y = event.pos
+                    offset_x = rectangle.x - mouse_x
+                    offset_y = rectangle.y - mouse_y
+
+        elif event.type == pygame.MOUSEBUTTONUP:
+            if event.button == 1:
+                rectangle_draging = False
+                print("(x, y) = (" + str(rectangle.x) + ", " + str(rectangle.y) + ")")
 
 
-    screen.fill(black)
-    screen.blit(pin, ballrect)
+        elif event.type == pygame.MOUSEMOTION:
+            if rectangle_draging:
+                mouse_x, mouse_y = event.pos
+                rectangle.x = mouse_x + offset_x
+                rectangle.y = mouse_y + offset_y
+
+    # - updates (without draws) -
+
+    # empty
+
+    # - draws (without updates) -
+
+    #screen.fill(WHITE)
+    screen.blit(map, (0,0))
+    pygame.draw.rect(screen, RED, rectangle)
+
+
+    #pygame.draw.rect(screen, rectangle)
+
     pygame.display.flip()
+
+    # - constant game speed / FPS -
+
+    clock.tick(FPS)
+
+# - end -
+
+pygame.quit()
